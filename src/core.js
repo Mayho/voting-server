49cac30ec0edbe3b93b67cb872853631d49af28b
Mehul Patel <mehul8@gmail.com>
2015-12-16 23:46:02 -0800

    update logic to pass specs

--- src/core.js
+++ src/core.js
@@ -1,9 +1,10 @@
 import {List, Map} from 'immutable';
 
+export const INITIAL_STATE = Map();
+
 export function setEntries(state, entries) {
   return state.set('entries', List(entries));
 }
-
 
 function getWinners(vote) {
   if (!vote) return [];
